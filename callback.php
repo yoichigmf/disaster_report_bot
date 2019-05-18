10de1b8113c96f9655947e602a5e36950bf2dc7c
yoichi_gmf <yoichi.kayama@gmail.com>
2019-05-18 15:06:44 +0900

    debug

--- callback.php
+++ callback.php
@@ -190,7 +190,7 @@ function upload_contents( $kind , $ext, $content_type, $response ) {  // ファ�
             
   
              $url = "https://content.dropboxapi.com/2/files/upload";      
-             $tgfilename = "/disasterinfo/${filename}";
+             $tgfilename = "/disasterinfo/${kind}/${filename}";
              
              $filearg = "Dropbox-API-Arg: {\"path\":\"${tgfilename}\"}";
         
