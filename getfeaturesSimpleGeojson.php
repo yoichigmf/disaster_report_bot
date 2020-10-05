99b0745513056a97ad6c00ebea27363b97422300
yoichi_gmf <yoichi.kayama@gmail.com>
2020-10-06 04:04:48 +0900

    debug

--- getfeaturesSimpleGeojson.php
+++ getfeaturesSimpleGeojson.php
@@ -17,9 +17,9 @@ header("Content-Type: application/json; charset=UTF-8"); //ヘッダー情報の
 
 
 
- $sheetname = filter_input(INPUT_POST,"sheetname"); //変数の出力。jQueryで指定したキー値optを用いる
+ $sheetname = filter_input(INPUT_GET,"sheetname"); //変数の出力。jQueryで指定したキー値optを用いる
 
- $sheetid= filter_input(INPUT_POST,"sheetid"); //変数の出力。jQueryで指定したキー値optを用いる
+ $sheetid= filter_input(INPUT_GET,"sheetid"); //変数の出力。jQueryで指定したキー値optを用いる
 
 //$sheetname = "Copy";
 //  add  
